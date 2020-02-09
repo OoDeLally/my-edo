4f96170aa652d9a2a2e0a546e5744ca76ac93f22
Pascal Heitz <heitz.pascal@gmail.com>
2020-02-09 19:27:28 +0100

    Degree count from note name list

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,30 @@ import classNames from 'classnames';
 import './App.css';
 
 const CENTS_IN_OCTAVE = 1200;
-const DEGREE_SIZE_IN_CENTS = 100;
-const DEGREE_COUNT = CENTS_IN_OCTAVE / DEGREE_SIZE_IN_CENTS;
 const FREQUENCY_OFFSET = 16.35; // C0
 const NOTE_NAMES = [
-  'C', 'K', 'D', 'L', 'E', 'F', 'M', 'G', 'P', 'A', 'H', 'B',
+  // 'C', 'K', 'D', 'L', 'E', 'F', 'M', 'G', 'P', 'A', 'H', 'B',
   //
   // 'C', '', 'D', '', 'E', 'F', '', 'G', '', 'A', '', 'B',
   // Alphabetic:
   // 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
   // Western:
-  // 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B',
+  'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B',
 ];
-
-if (NOTE_NAMES.length !== DEGREE_COUNT) {
-  throw new Error(`There are ${DEGREE_COUNT} divisions but only ${NOTE_NAMES.length} names.`);
-}
+const DEGREE_COUNT = NOTE_NAMES.length;
+const DEGREE_SIZE_IN_CENTS = CENTS_IN_OCTAVE / DEGREE_COUNT;
 
 const INTERCENT_FACTOR = Math.pow(2, 1 / CENTS_IN_OCTAVE);
 
 
 const getNoteName = (cents: number) => {
-  const octaveNum = Math.floor(cents / CENTS_IN_OCTAVE);
-  const note = NOTE_NAMES[(cents % CENTS_IN_OCTAVE) / DEGREE_SIZE_IN_CENTS];
+  const roundedCents = Math.round(cents);
+  const octaveNum = Math.floor(roundedCents / CENTS_IN_OCTAVE);
+  const degree = (roundedCents % CENTS_IN_OCTAVE) / DEGREE_SIZE_IN_CENTS;
+  const note = NOTE_NAMES[Math.round(degree)];
+  if (!note) {
+    throw new Error(`Could not find name for degree ${degree}`)
+  }
   return `${note}${octaveNum}`;
 };
 
